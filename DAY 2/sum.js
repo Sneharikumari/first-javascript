226e7749281ff3fdd02a7f928267ca5336a76d37
my first project <snehakumari92343@gmail.com>
2026-05-19 13:22:54 +0530

    using function number

--- DAY 2/sum.js
+++ DAY 2/sum.js
@@ -1,1 +1,12 @@
-function sum(num) {}
+function sum(n) {
+  let count = 0;
+
+  for (let i = 1; i <= n; i++) {
+    console.log("count:", count, " +", i, "=", count + i);
+    count = count + i;
+  }
+
+  console.log(count);
+}
+
+sum(5);
