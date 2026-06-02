b67d8afcd2e929063599e4c3c447c384ee23fcb1
my first project <snehakumari92343@gmail.com>
2026-06-02 14:37:12 +0530

    using  sever1  number

--- server/index.js
+++ server/index.js
@@ -1,82 +1,31 @@
-const { log } = require("node:console");
 const news = require("./dataset/news");
 const express = require("express");
 const app = express();
-// app.get("/news", (req, res) => {
-//   res.send(news);
-// });
+const { searchByTerm, searchByCategory } = require("./modules/utils");
 
-// app.get("/news/:id", (req, res) => {
-//   const id = req.params.id;
-//   res.send(id);
-// });
-// app.listen(3000);
+app.get("/", (req, res) => {
+  return res.send(" Welcome to Our News Website");
+});
 
-// const name = "preety";
-// app.get("/sneha", (req, res) => {
-//   res.send(name);
-// });
+app.get("/news", (req, res) => {
+  const { search, category, limit } = req.query;
+  let filternews = news;
+  if (search) {
+    filternews = searchByTerm(search, news);
+  }
+  if (category) {
+    filternews = searchByCategory(category, news);
+  }
+  if (limit) {
+    filternews = filternews.slice(0, limit);
+  }
+  if (filternews.length > 0) {
+    return res.send(filternews);
+  } else {
+    return res.send("News Not Found, please try some different filters");
+  }
+});
 
-// const picnic = "dalma";
-// app.get("/place", (req, res) => {
-//   res.send("dalma");
-// });
-
-// app.get("/news/:id", (req, res) => {
-//   const id = req.params.id;
-//   for (let i = 0; i < news.length; i++) {
-//     if (news[i].id == id) {
-//       return res.send(news[i]);
-//     }
-//   }
-// });
-// app.listen(3000);
-
-// app.get("/news", (req, res) => {
-//   const category = req.query.category;
-//   if (!category) {
-//     return res.send(news);
-//   }
-//   const result = news.filter((item) => item.category === category);
-//   res.send(result);
-// });
-// app.listen(3000);
-
-// app.get("/news", (req, res) => {
-//   const category = req.query.category;
-//   const limit = req.query.limit;
-//   console.log(category, limit);
-//   let result = news;
-//   if (category) {
-//     result = result.filter((i) => i.category === category);
-//   }
-//   if (limit) {
-//     result = result.slice(0, Number(limit));
-//   }
-//   res.send(result);
-// });
-// app.listen(3000);
-
-// app.get("/news", (req, res) => {
-//   const limit = Number(req.query.limit);
-//   const from = req.query.from;
-//   const category = req.query.category;
-//   console.log(from, limit);
-//   let result = news;
-//   if (limit) {
-//     if (from === "bottom") {
-//       result = result.slice(-limit);
-//     } else {
-//       result = result.slice(0, limit);
-//     }
-//   }
-//   res.send(result);
-// });
-
-// const express = require("express");
-// const app = express();
-// app.get("/", (req, res) => {
-//   res.send("news API is running");
-// });
-
-// app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
---
+++ server/index2.js
@@ -0,0 +1,102 @@
+const { log } = require("node:console");
+const news = require("./dataset/news");
+// const express = require("express");
+//  const app = express();
+// app.get("/news", (req, res) => {
+//   res.send(news);
+// });
+// app.listen(3000);
+
+// app.get("/news/:id", (req, res) => {
+//   const id = req.params.id;
+//   res.send(id);
+// });
+// app.listen(3000);
+
+// const name = "preety";
+// app.get("/sneha", (req, res) => {
+//   res.send(name);
+// });
+
+// const picnic = "dalma";
+// app.get("/place", (req, res) => {
+//   res.send("dalma");
+// });
+
+// app.get("/news/:id", (req, res) => {
+//   const id = req.params.id;
+//   for (let i = 0; i < news.length; i++) {
+//     if (news[i].id == id) {
+//       return res.send(news[i]);
+//     }
+//   }
+// });
+//app.listen(3000);
+
+// app.get("/news", (req, res) => {
+//   const category = req.query.category;
+//   if (!category) {
+//     return res.send(news);
+//   }
+//   const result = news.filter((item) => item.category === category);
+//   res.send(result);
+// });
+// app.listen(3000);
+
+// app.get("/news", (req, res) => {
+//   const category = req.query.category;
+//   const limit = req.query.limit;
+//   console.log(category, limit);
+//   let result = news;
+//   if (category) {
+//     result = result.filter((i) => i.category === category);
+//   }
+//   if (limit) {
+//     result = result.slice(0, Number(limit));
+//   }
+//   res.send(result);
+// });
+// app.listen(3000);
+
+// app.get("/news", (req, res) => {
+//   const limit = Number(req.query.limit);
+//   const from = req.query.from;
+//   const category = req.query.category;
+//   console.log(from, limit);
+//   let result = news;
+//   if (limit) {
+//     if (from === "bottom") {
+//       result = result.slice(-limit);
+//     } else {
+//       result = result.slice(0, limit);
+//     }
+//   }
+//   res.send(result);
+// });
+// app.listen(3000);
+
+// const express = require("express");
+// const app = express();
+// app.get("/", (req, res) => {
+//   res.send("news API is running");
+// });
+
+// app.listen(3000);
+
+// const express = require("express");
+// const { title } = require("node:process");
+// const app = express();
+
+// app.get("/news", (req, res) => {
+//   const search = req.query.search;
+//   if (!search) {
+//     return res.json(news);
+//   }
+//   const result = news.filter(
+//     (item) =>
+//       item.newstitle &&
+//       item.newstitle.toLowerCase().includes(search.toLocaleLowerCase()),
+//   );
+//   res.json(result);
+// });
+// app.listen(3000);
