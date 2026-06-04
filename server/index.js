369b43d36475fa9843aa42bdb091694726891c25
my first project <snehakumari92343@gmail.com>
2026-06-04 17:53:52 +0530

    using  sever2  number

--- server/index.js
+++ server/index.js
@@ -1,31 +1,24 @@
-const news = require("./dataset/news");
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const api = require("./config/prisma");
 const app = express();
-const { searchByTerm, searchByCategory } = require("./modules/utils");
 
-app.get("/", (req, res) => {
-  return res.send(" Welcome to Our News Website");
+app.use(cors());
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await api.user.findMany();
+  res.json(users);
 });
 
-app.get("/news", (req, res) => {
-  const { search, category, limit } = req.query;
-  let filternews = news;
-  if (search) {
-    filternews = searchByTerm(search, news);
-  }
-  if (category) {
-    filternews = searchByCategory(category, filternews);
-  }
-  if (limit) {
-    filternews = filternews.slice(0, limit);
-  }
-  if (filternews.length > 0) {
-    return res.send(filternews);
-  } else {
-    return res.send("News Not Found, please try some different filters");
-  }
+app.post("/users", async (req, res) => {
+  console.log(req.body);
+  const { email, name } = req.body;
+  const user = await api.user.create({ data: { email, name } });
+  res.status(201).json(user);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server on http://localhost:${PORT}`));
